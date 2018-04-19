6c091f0745468ec269b2413d510d714992c665af
Joe Fleming <joe.fleming@gmail.com>
2018-04-19 14:56:37 -0700

    chore: rename public methods
    BREAKING CHANGE: public api renamed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const githubAuthors = require('./github_authors');
 const gitAuthors = require('./git_authors');
 
 module.exports = {
-  authors: githubAuthors,
-  nameAndEmail: gitAuthors,
+  githubAuthors,
+  gitAuthors,
 };
