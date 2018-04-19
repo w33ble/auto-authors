46cf6a221757ce3c8df1341247d9022867c75164
Joe Fleming <joe.fleming@gmail.com>
2018-04-19 15:42:34 -0700

    feat: add handlebars output support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const githubAuthors = require('./github_authors');
 const gitAuthors = require('./git_authors');
+const compileTemplate = require('./template');
 
 module.exports = {
   githubAuthors,
   gitAuthors,
+  compileTemplate,
 };
---
+++ src/template.js
@@ -0,0 +1,40 @@
+const { readFile } = require('fs');
+const { join } = require('path');
+const Handlebars = require('handlebars');
+
+function removeIndentation(string) {
+  return string.replace(/\n +/g, '\n').replace(/^ +/, '');
+}
+
+function getTemplate(template, cb) {
+  // attempt to read from file
+  readFile(template, 'utf8', (err, content) => {
+    if (err) {
+      // no match, attempt to load provided templates
+      const templatePath = join('templates', `${template}.hbs`);
+      readFile(templatePath, 'utf8', cb);
+      return;
+    }
+
+    // return loaded template file
+    cb(null, content);
+  });
+}
+
+module.exports = function compileTemplate(tpl, data) {
+  if (tpl === 'json') {
+    return JSON.stringify(data, null, 2);
+  }
+
+  return new Promise((resolve, reject) => {
+    getTemplate(tpl, (err, template) => {
+      if (err) {
+        if (err.code === 'ENOENT') reject(new Error(`Template not found: ${tpl}`));
+        else reject(err);
+      } else resolve(template);
+    });
+  }).then(template => {
+    const output = Handlebars.compile(template)(data);
+    return removeIndentation(output);
+  });
+};
